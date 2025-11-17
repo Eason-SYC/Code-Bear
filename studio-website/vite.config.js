0dc14c8f048da2cc4eccf51df0ec214620abe78e
Eason-SYC <19820636768@163.com>
2025-11-17 21:50:34 +0800

    update

--- studio-website/vite.config.js
+++ studio-website/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Code-bear/' : '/', // 根据环境变量动态设置 base 路径
+  base: '/', // Set base path to '/' unconditionally
   assetsInclude: ['**/*.svg'],
   build: {
     rollupOptions: {
